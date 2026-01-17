e5f0640efe8c564718277af4edaca2daa870551d
hojeong <parkhj062@gmail.com>
2026-01-17 14:01:00 +0900

    [23. 테스트 - 데이터베이스 분리]
    - use another db for test

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest // find SpringBootApplication
 class ItemRepositoryTest {
 
     @Autowired
